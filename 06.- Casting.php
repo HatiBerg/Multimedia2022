1c273d7341eadbdfbd462d7fa1652d558a50a55a
Esteban Adolfo Ponce Lira <esteban.ponce@alumnos.upla.cl>
2022-09-01 12:02:23 -0400

    Clase 01-09

---
+++ 06.- Casting.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Casting
+Es cuando forzamos a un tipo de dato que se convierta en otro
+*/
+
+$numero_string = "J"; //String
+var_dump($numero_string);
+echo "<br>";
+
+$numero_int = (int) $numero_string; //Integer
+var_dump($numero_int);
+echo "<br>";
+
+$numero_float = (float) $numero_string;
+var_dump($numero_float);
+echo "<br>";
+
+$dias = 5.85;
+$dias = (int) $dias; //Integer
+var_dump($dias);
+echo "<br>";
+
+$bandera = 0;
+$bandera = (bool) $bandera;
+var_dump($bandera);
+echo "<br>";
