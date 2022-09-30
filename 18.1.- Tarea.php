938d4bc59d55fece5fd424519bf90b6653b15397
Esteban Adolfo Ponce Lira <esteban.ponce@alumnos.upla.cl>
2022-09-30 14:31:59 -0300

    Clase 22-09

--- 18.1.- Tarea.php
+++ 18.1.- Tarea.php
@@ -1,3 +1,10 @@
+<?php
+
+/*
+1) Crear un formulario que envíe un nombre, apellido y foto a un archivo de PHP que verifique que se esta mandando una imagen y no otro tipo de archivo. Y guardar esos datos en la base de datos, en una tabla que tenga el id, nombre, apellido y la ruta del archivo que se envió.
+*/
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,15 +14,17 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="18.2.-Tarea"method="POST">
+    <form action="18.2- Tarea.php" method="POST" enctype="multipart/form-data">
         <label for="nombre">Nombre: </label>
         <input type="text" id="nombre" name="nombre">
         <br>
         <label for="apellido">Apellido: </label>
         <input type="text" id="apellido" name="apellido">
         <br>
-        <label for="Foto">Foto: </label>
-        <input type="text" id="url_foto" name="url_foto">
+        <label for="foto">Foto: </label>
+        <input type="file" id="foto" name="foto">
         <br>
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
